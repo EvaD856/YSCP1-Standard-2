30474f8ec8d6204b09c22e264280a5d7878b68cb
EvaD856 <148793536+EvaD856@users.noreply.github.com>
2025-02-26 15:48:26 +0000

    end of day 1 work

--- tip_calc.py
+++ tip_calc.py
@@ -1,6 +1,9 @@
 # tip_calculator.py
 # A simple tip calculator to compute tip amount and total bill
-# REMEMBER TO REMOVE COMMENTS TO SELF B4 SUBMISSION
+
+# REMEMBER TO REMOVE COMMENTS TO SELF B4 FINAL SUBMISSION
+
+#Main Issue: Parameters/Variables, structures should be like 90% fine
 
 #Asks for the bill amount
 def bill_amount():
@@ -8,7 +11,7 @@ def bill_amount():
         try:
             bill = float(input("Enter the bill amount: $"))
             return bill
-        except: # i know the except correct
+        except: # i know the except correct from what I remember in other assignment
             print("Error")
             bill_amount()
     
@@ -26,23 +29,25 @@ def tip_percentage():
 
 # #Calculates the tip amount
 def calculate_tip(bill_amount, tip_percentage): #Used right parameters just isn't working??
-    calculate_tip = bill * tip #this prolly wrong
-    return calculate_tip
+    calculate = bill * tip #this prolly wrong?
+    return calculate
 
 
-#Should all be correct or easily typed correctly
+#Redo the two lines
 def display_summary(bill_amount, tip_percentage, calculate_tip): # This I'm 99% sure is correct parameters, or need new set of vars(?)
     print("Bill Summary")
-#  print(bill_amount) #basic stuff don't wory till later for print but format is print(f"Total is {total:.2f} sjndfksjdf")
-#    print(tip_percentage)
-
+    print(f"Bill Amount:{bill_amount:.2f}")#should be correct format
+    print(f"Tip Amount:{calculate_tip:.2f}")
+    total_bill = calculate + bill_amount #may have to go back to this
+    print(f"Total Bill:${total_bill:.2f}") #also go back to
+    print("Thank you for using the Tip Calculator!")
 
 def main():
     print("Welcome to the Tip Calculator!")
     bill_amount()
     tip_percentage(tip)
-    calculate_tip(bill_amount, tip_percentage)
-    # display_summary(bill_amount, tip_percentage, calculate_tip)
+    # calculate_tip(bill_amount, tip_percentage)
+    display_summary(bill_amount, tip_percentage)# calculate_tip)
 
 #Calls main function
 main()
